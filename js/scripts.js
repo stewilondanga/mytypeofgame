0892c79876086f15a9c2ed5215466814c44c6bd0
Stewart ilondanga <stilondanga@gmail.com>
2018-09-14 14:44:03 +0300

    add js/scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -303,40 +303,40 @@ button.addEventListener("click", function(e) {
 });
 
 
-/*      	function typing(e) {
-      			typed = String.fromCharCode(e.which);
-      			for (var i = 0; i < spans.length; i++) {
-      				if (spans[i].innerHTML === typed) { // if typed letter is the one from the word
-      					if (spans[i].classList.contains("bg")) { // if it already has class with the bacground color then check the next one
-      						continue;
-      					} else if (spans[i].classList.contains("bg") === false && spans[i-1] === undefined || spans[i-1].classList.contains("bg") !== false ) { // if it dont have class, if it is not first letter or if the letter before it dont have class (this is done to avoid marking the letters who are not in order for being checked, for example if you have two "A"s so to avoid marking both of them if the first one is at the index 0 and second at index 5 for example)
-      						spans[i].classList.add("bg");
-      						break;
-      					}
-      				}
-      			}
-      			var checker = 0;
-      			for (var j = 0; j < spans.length; j++) { //checking if all the letters are typed
-      				if (spans[j].className === "span bg") {
-      					checker++;
-      				}
-      				if (checker === spans.length) { // if so, animate the words with animate.css class
-                spark.pause();
-    					  spark.currentTime = 0;
-                spark.play();
-      					words.classList.add("animated");
-      					words.classList.add("fadeOut");
-      					points++; // increment the points
-      					scoreDiv.innerHTML = points; //add points to the points div
-      					document.removeEventListener("keydown", typing, false);
-      					setTimeout(function(){
-      						words.className = "words"; // restart the classes
-      						random(); // give another word
-      						document.addEventListener("keydown", typing, false);
-      					}, 400);
-      				}
+function typing(e) {
+  typed = String.fromCharCode(e.which);
+  for (var i = 0; i < spans.length; i++) {
+    if (spans[i].innerHTML === typed) { // if typed letter is the one from the word
+      if (spans[i].classList.contains("bg")) { // if it already has class with the bacground color then check the next one
+        continue;
+      } else if (spans[i].classList.contains("bg") === false && spans[i - 1] === undefined || spans[i - 1].classList.contains("bg") !== false) { // if it dont have class, if it is not first letter or if the letter before it dont have class (this is done to avoid marking the letters who are not in order for being checked, for example if you have two "A"s so to avoid marking both of them if the first one is at the index 0 and second at index 5 for example)
+        spans[i].classList.add("bg");
+        break;
+      }
+    }
+  }
+  /*      			var checker = 0;
+        			for (var j = 0; j < spans.length; j++) { //checking if all the letters are typed
+        				if (spans[j].className === "span bg") {
+        					checker++;
+        				}
+        				if (checker === spans.length) { // if so, animate the words with animate.css class
+                  spark.pause();
+      					  spark.currentTime = 0;
+                  spark.play();
+        					words.classList.add("animated");
+        					words.classList.add("fadeOut");
+        					points++; // increment the points
+        					scoreDiv.innerHTML = points; //add points to the points div
+        					document.removeEventListener("keydown", typing, false);
+        					setTimeout(function(){
+        						words.className = "words"; // restart the classes
+        						random(); // give another word
+        						document.addEventListener("keydown", typing, false);
+        					}, 400);
+        				}
 
-      			}
-      	}
+        			}
+        	}
 
-      	document.addEventListener("keydown", typing, false);
+        	document.addEventListener("keydown", typing, false);
